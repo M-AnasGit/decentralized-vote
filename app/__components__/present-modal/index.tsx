5c226be6ba84a145694e1e298bcabf3dfacbbd78
Anas <manasgithub1@gmail.com>
2025-04-30 13:04:37 +0100

    refactor: refactor main page to work with new providers

---
+++ app/__components__/present-modal/index.tsx
@@ -0,0 +1,45 @@
+'use client';
+import React from 'react';
+
+import { toast } from 'sonner';
+
+import GenericModal from '@/components/generic-modal';
+import { Input } from '@/components/ui/input';
+
+type Props = {
+    candidates: any[];
+    handlePresentAsCandidate: (name: string) => Promise<string | null>;
+};
+
+export default function PresentModal({ candidates, handlePresentAsCandidate }: Props) {
+    const [name, setName] = React.useState<string>('');
+    const confirmVote = async () => {
+        if (!name) {
+            alert('Please select a candidate before voting!');
+            return;
+        }
+
+        if (candidates.some((candidate) => candidate.name === name)) {
+            alert('This name is already taken! Please choose another one.');
+            return;
+        }
+
+        toast.info('Proceeding... Please wait.', {
+            duration: 2000000,
+        });
+        const txId = await handlePresentAsCandidate(name);
+        toast.dismiss();
+        if (!txId) {
+            alert('Transaction failed. You have already voted!');
+            return;
+        } else {
+            toast.success('Transaction successful! You are now a candidate.');
+        }
+    };
+
+    return (
+        <GenericModal title="Become a candidate" description="Become a candidate so that other users can choose you" btn_text="Confirm" btn_action={confirmVote}>
+            <Input name="name" type="text" placeholder="Enter your candidate name" value={name} onChange={(e) => setName(e.target.value)} />
+        </GenericModal>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import Image from 'next/image';
 
 import ProcessBubble from './__components__/process-bubble';
 import VoteModal from './__components__/vote-modal';
+import ResultModal from './__components__/result-modal';
+import PresentModal from './__components__/present-modal';
+
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../components/ui/tooltip';
 import { Button } from '../components/ui/button';
 
@@ -13,7 +16,7 @@ import { ScrollText, SquareCheckBig, Vote, Wallet } from 'lucide-react';
 import { useUserData } from '@/services/user/provider';
 import { useModal } from '@/services/modal/provider';
 import { useBlockchain } from '@/services/blockchain/provider';
-import ResultModal from './__components__/result modal';
+import { toast } from 'sonner';
 
 const shortenAddress = (address: string) => {
     return `${address.slice(0, 4)}...${address.slice(-4)}`;
@@ -21,14 +24,30 @@ const shortenAddress = (address: string) => {
 
 export default function Home() {
     const { user, connectWallet, disconnectWallet } = useUserData();
-    const { handleBlockchainVote, handleGetVotes } = useBlockchain();
+    const { candidates, handleBlockchainVote, handleGetVoteResults, handlePresentAsCandidate, handleRemoveCandidate } = useBlockchain();
     const { handleSetModal } = useModal();
 
     const handleVote = () => {
-        handleSetModal(<VoteModal handleVote={handleBlockchainVote} />);
+        handleSetModal(<VoteModal candidates={candidates} handleVote={handleBlockchainVote} />);
     };
     const handleResults = () => {
-        handleSetModal(<ResultModal handleGetVotes={handleGetVotes} />);
+        handleSetModal(<ResultModal handleGetVotes={handleGetVoteResults} />);
+    };
+    const handleBecomeCandidate = () => {
+        handleSetModal(<PresentModal candidates={candidates} handlePresentAsCandidate={handlePresentAsCandidate} />);
+    };
+    const handleResign = async () => {
+        toast.info('Resigning... Please wait.', {
+            duration: 2000000,
+        });
+        const response = await handleRemoveCandidate();
+        toast.dismiss();
+        if (!response) {
+            alert('Transaction failed. You have already voted!');
+            return;
+        } else {
+            toast.success('Transaction successful! You are no longer a candidate.');
+        }
     };
 
     return (
@@ -64,11 +83,12 @@ export default function Home() {
                             Decentralized <br></br>Voting
                         </h1>
                         <p className="text-lg font-medium text-muted-foreground">Vote for the right candidate!</p>
+                        {!user && <p className="text-center font-medium text-destructive">Please connect your wallet to vote.</p>}
                         <div className="flex gap-4 my-4">
-                            <Button variant="default" onClick={handleVote}>
+                            <Button variant="default" onClick={handleVote} disabled={!user}>
                                 Vote now
                             </Button>
-                            <Button variant="secondary" onClick={handleResults}>
+                            <Button variant="secondary" onClick={handleResults} disabled={!user}>
                                 Result
                             </Button>
                         </div>
@@ -80,6 +100,20 @@ export default function Home() {
                     <ProcessBubble Icon={SquareCheckBig} title="Register your vote" />
                     <ProcessBubble Icon={Vote} title="Confirm and submit" />
                     <ProcessBubble Icon={ScrollText} title="Check the results" />
+                </div>
+                <h2 className="text-center text-3xl text-primary font-medium">How to become a candidate</h2>
+                <div className="bg-muted text-muted-foreground p-4 rounded-md w-1/2 mx-auto flex flex-col gap-4 items-center justify-center">
+                    <p className="text-center text-lg">
+                        To become a candidate, you need to register your name while connected to your wallet. Once registered, you can start receiving votes from users. <br></br>
+                    </p>
+                    <div className="flex gap-4">
+                        <Button variant="default" onClick={handleBecomeCandidate} disabled={!user}>
+                            Become a candidate
+                        </Button>
+                        <Button variant="secondary" onClick={handleResign} disabled={!user}>
+                            Resign
+                        </Button>
+                    </div>
                 </div>
             </main>
         </div>
